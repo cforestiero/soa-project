c2a887cb897647d9e3aab1f34dd08e2d54140aca
Camila Forestiero <cforestiero@alumno.unlam.edu.ar>
2024-06-21 19:12:36 -0300

    Add pump action message

--- Android/app/src/main/java/com/example/smartpool/MainActivity.java
+++ Android/app/src/main/java/com/example/smartpool/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.smartpool;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,7 +8,8 @@ import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -23,11 +23,14 @@ import java.lang.ref.WeakReference;
 public class MainActivity extends AppCompatActivity {
 
     private static final String PUMP_MODE = "B";
+    private static final String PUMP_WORKING = "W";
 
     private BluetoothManager bluetoothManager;
 
     Button buttonLights;
     Button buttonDewater;
+    TextView rectanglePumpActionTextView;
+    ImageView boy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,17 +45,30 @@ public class MainActivity extends AppCompatActivity {
 
         buttonLights = findViewById(R.id.button);
         buttonDewater = findViewById(R.id.buttonDewater);
+        rectanglePumpActionTextView = findViewById(R.id.rectangleTextView);
+        boy = findViewById(R.id.boy);
 
         bluetoothManager = BluetoothManager.getInstance(new WeakReference<>(this), this);
         bluetoothManager.setContext(this);
         bluetoothManager.setHandler(bluetoothIn);
         bluetoothManager.sendCommand(PUMP_MODE);
+        // Cada cierto tiempo se mande a ver si se esta filtrando o desagotando
+        bluetoothManager.sendCommand(PUMP_WORKING);
 
         buttonLights.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, LightActivity.class));
             }
+        });
+
+        boy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cada vez que apreto al hombrecito se mandan mensajes para actualizar el Main Activity
+                // despues hacer que Cada cierto tiempo se manden estos comandos a ver si se esta filtrando o desagotando
+                bluetoothManager.sendCommand(PUMP_MODE);
+                bluetoothManager.sendCommand(PUMP_WORKING);            }
         });
     }
 
@@ -87,6 +103,20 @@ public class MainActivity extends AppCompatActivity {
                 if (value.contains("Filtrado")){
                     // Si la bomba esta en modo filtrado entonces se esconde el boton de desagote
                     buttonDewater.setVisibility(View.GONE);
+                }
+                break;
+            case PUMP_WORKING:
+                Log.d("MainActivity", "Handling PUMP_WORKING with value: " + value);
+                rectanglePumpActionTextView.setText(value);
+                rectanglePumpActionTextView.setVisibility(TextView.VISIBLE);
+               // if (value.contains("Filtrando")){
+                 //   rectanglePumpActionTextView.setText(value);
+                   // rectanglePumpActionTextView.setVisibility(TextView.VISIBLE);
+                    // Si la bomba esta Filtrando se muestra un coso que dice filtrando
+                    //buttonDewater.setVisibility(View.);
+                //}
+                if (value.contains("NO")){
+                    rectanglePumpActionTextView.setVisibility(TextView.GONE);
                 }
                 break;
             default:
