2fa69b18c9994eacc12f8ee3f354dc9a12585491
Camila Forestiero <cforestiero@alumno.unlam.edu.ar>
2024-06-22 20:48:30 -0300

    Add Bluetooth in dewater

--- Android/app/src/main/java/com/example/smartpool/DewaterActivity.java
+++ Android/app/src/main/java/com/example/smartpool/DewaterActivity.java
@@ -2,14 +2,26 @@ package com.example.smartpool;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.lang.ref.WeakReference;
+
 public class DewaterActivity extends AppCompatActivity {
+
+    private static final String DEWATER_SIGNAL_READY = "D";
+    private BluetoothManager bluetoothManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +38,16 @@ public class DewaterActivity extends AppCompatActivity {
         }
 
         Button confirmButton = findViewById(R.id.button2);
+
+        bluetoothManager = BluetoothManager.getInstance(new WeakReference<>(this), this);
+        bluetoothManager.setContext(this);
+        bluetoothManager.setHandler(bluetoothIn);
+
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Aquí puedes agregar código para realizar alguna acción antes de volver a MainActivity
-                // Por ejemplo, guardar datos, realizar operaciones, etc.
+                // Se envia command para que la bomba ande
+                bluetoothManager.sendCommand(DEWATER_SIGNAL_READY);
 
                 // Luego, puedes volver a MainActivity
                 Intent intent = new Intent(DewaterActivity.this, MainActivity.class);
@@ -38,6 +55,14 @@ public class DewaterActivity extends AppCompatActivity {
                 finish(); // Esto evita que la actividad actual quede en el stack
             }
         });
-
     }
+
+    final Handler bluetoothIn = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            String receivedMessage = (String) msg.obj;
+            Log.d("DewaterActivity", "Received message: " + receivedMessage);
+
+        }};
+
 }
